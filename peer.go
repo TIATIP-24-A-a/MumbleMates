9bbc35127193f13030cfc38a5a9fcc677e39a014
Tim Ineichen <tim.ineichen@synprovis.ch>
2025-02-11 10:36:20 +0100

    feat(chat): changed to a OOP approach

--- peer.go
+++ peer.go
@@ -1,127 +1,158 @@
 package main
 
 import (
-    "bufio"
-    "context"
-    "fmt"
-    "os"
-    "os/signal"
-    "strings"
-    "syscall"
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 
-    "github.com/libp2p/go-libp2p"
-    peerstore "github.com/libp2p/go-libp2p/core/peer"
-    "github.com/libp2p/go-libp2p/core/protocol"
-    network "github.com/libp2p/go-libp2p/core/network"
-    multiaddr "github.com/multiformats/go-multiaddr"
+	"github.com/libp2p/go-libp2p"
+	host "github.com/libp2p/go-libp2p/core/host"
+	network "github.com/libp2p/go-libp2p/core/network"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	multiaddr "github.com/multiformats/go-multiaddr"
 )
 
-func handleStream(stream network.Stream) {
-    defer stream.Close()
-    buf := bufio.NewReader(stream)
-    for {
-        message, err := buf.ReadString('\n')
-        if err != nil {
-            if err.Error() == "EOF" {
-                fmt.Println("stream closed by remote peer")
-                break
-            } else {
-                fmt.Println("error reading from stream:", err)
-                break
-            }
-        }
-        // Print only messages received from remote peer
-        fmt.Println("You:", strings.TrimSpace(message)) // display as "You: <message>"
-    }
+type ChatNode struct {
+	Node   host.Host
+	Stream network.Stream
 }
 
-func handleUserInput(stream network.Stream) {
-    reader := bufio.NewReader(os.Stdin)
-    for {
-        fmt.Print("Me: ")
-        message, _ := reader.ReadString('\n')
-        message = strings.TrimSpace(message)
-        if message == "" {
-            continue
-        }
+func NewChatNode() (*ChatNode, error) {
+	node, err := libp2p.New(libp2p.ListenAddrStrings())
+	if err != nil {
+		return nil, err
+	}
+	return &ChatNode{
+		Node: node,
+	}, nil
+}
 
-        // Send the typed message to the remote peer over the stream
-        if stream != nil {
-            _, err := stream.Write([]byte(message + "\n"))
-            if err != nil {
-                fmt.Println("error writing to stream:", err)
-                // Handle stream reset or closing
-                if err.Error() == "stream reset" {
-                    fmt.Println("stream reset detected, closing stream.")
-                    stream.Close()
-                    return
-                }
-            } else {
-                // After sending, print the message sent
-                fmt.Println("Me:", message) // display as "Me: <message>"
-            }
-        }
-    }
+func (c *ChatNode) GetAddress() (string, error) {
+	peerInfo := peerstore.AddrInfo{
+		ID:    c.Node.ID(),
+		Addrs: c.Node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		return "", err
+	}
+	return addrs[0].String(), nil
+}
+
+func (c *ChatNode) ConnectToPeer(address string) error {
+	addr, err := multiaddr.NewMultiaddr(address)
+	if err != nil {
+		return err
+	}
+	peer, err := peerstore.AddrInfoFromP2pAddr(addr)
+	if err != nil {
+		return err
+	}
+	if err := c.Node.Connect(context.Background(), *peer); err != nil {
+		return err
+	}
+	fmt.Println("connected to", address)
+	c.Stream, err = c.Node.NewStream(context.Background(), peer.ID, protocol.ID("/chat/1.0.0"))
+	return err
+}
+
+func (c *ChatNode) HandleStream(stream network.Stream) {
+	defer stream.Close()
+	buf := bufio.NewReader(stream)
+	for {
+		message, err := buf.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println("stream closed by remote peer")
+				break
+			} else {
+				fmt.Println("error reading from stream:", err)
+				break
+			}
+		}
+		// Print only messages received from remote peer
+		fmt.Println("You:", strings.TrimSpace(message)) // display as "You: <message>"
+	}
+}
+
+func (c *ChatNode) HandleUserInput() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Me: ")
+		message, _ := reader.ReadString('\n')
+		message = strings.TrimSpace(message)
+		if message == "" {
+			continue
+		}
+
+		// Send the typed message to the remote peer over the stream
+		if c.Stream != nil {
+			_, err := c.Stream.Write([]byte(message + "\n"))
+			if err != nil {
+				fmt.Println("error writing to stream:", err)
+				// Handle stream reset or closing
+				if err.Error() == "stream reset" {
+					fmt.Println("stream reset detected, closing stream.")
+					c.Stream.Close()
+					return
+				}
+			} else {
+				// After sending, print the message sent
+				fmt.Println("Me:", message) // display as "Me: <message>"
+			}
+		}
+	}
+}
+
+func (c *ChatNode) Start() error {
+	// if a remote peer has been passed on the command line, connect to it
+	if len(os.Args) > 1 {
+		addr := os.Args[1]
+		if err := c.ConnectToPeer(addr); err != nil {
+			return err
+		}
+	} else {
+		// handle incoming streams
+		c.Node.SetStreamHandler(protocol.ID("/chat/1.0.0"), c.HandleStream)
+	}
+
+	// Start the user input handler in a separate goroutine
+	go c.HandleUserInput()
+
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+
+	// shut the node down
+	if err := c.Node.Close(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
-    // start a libp2p node that listens on a random local TCP port
-    node, err := libp2p.New(
-        libp2p.ListenAddrStrings(),
-    )
-    if err != nil {
-        panic(err)
-    }
+	chatNode, err := NewChatNode()
+	if err != nil {
+		panic(err)
+	}
 
-    // print the node's PeerInfo in multiaddr format
-    peerInfo := peerstore.AddrInfo{
-        ID:    node.ID(),
-        Addrs: node.Addrs(),
-    }
-    addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("libp2p node address:", addrs[0])
+	address, err := chatNode.GetAddress()
+	if err != nil {
+		panic(err)
+	}
 
-    var stream network.Stream
+	fmt.Println("libp2p node address:", address)
 
-    // if a remote peer has been passed on the command line, connect to it
-    if len(os.Args) > 1 {
-        addr, err := multiaddr.NewMultiaddr(os.Args[1])
-        if err != nil {
-            panic(err)
-        }
-        peer, err := peerstore.AddrInfoFromP2pAddr(addr)
-        if err != nil {
-            panic(err)
-        }
-        if err := node.Connect(context.Background(), *peer); err != nil {
-            panic(err)
-        }
-        fmt.Println("connected to", addr)
-
-        // open a stream to the remote peer
-        stream, err = node.NewStream(context.Background(), peer.ID, protocol.ID("/chat/1.0.0"))
-        if err != nil {
-            panic(err)
-        }
-    } else {
-        // handle incoming streams
-        node.SetStreamHandler(protocol.ID("/chat/1.0.0"), handleStream)
-    }
-
-    // Start the user input handler in a separate goroutine
-    go handleUserInput(stream)
-
-    // wait for a SIGINT or SIGTERM signal
-    ch := make(chan os.Signal, 1)
-    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-    <-ch
-    fmt.Println("Received signal, shutting down...")
-
-    // shut the node down
-    if err := node.Close(); err != nil {
-        panic(err)
-    }
+	// Start the chat node
+	if err := chatNode.Start(); err != nil {
+		panic(err)
+	}
 }
