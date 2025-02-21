abaa717dbc6873e621d291dc89a23f22d9f55926
Tuan Binh Tran <28620517+tuanbinhtran@users.noreply.github.com>
2025-02-21 13:58:51 +0100

    test: add test for create node

---
+++ internal/peer_test.go
@@ -0,0 +1,21 @@
+package chat
+
+import (
+	"testing"
+)
+
+func TestCreateNode(t *testing.T) {
+	node, err := NewChatNode("test")
+
+	if err != nil {
+		t.Errorf("Error creating node: %s", err)
+	}
+
+	if node == nil {
+		t.Errorf("Node shoud not be nil")
+	}
+
+	if node.name != "test" {
+		t.Errorf("Node name should be 'test', got %s", node.name)
+	}
+}
