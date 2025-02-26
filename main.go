dded05465faf06d6c93c85448ce8cbc4a0ff9630
Tuan Binh Tran <28620517+tuanbinhtran@users.noreply.github.com>
2025-02-26 20:58:01 +0100

    refactor: remove unused `askForName` func

--- main.go
+++ main.go
@@ -9,28 +9,6 @@ import (
 	"github.com/TIATIP-24-A-a/MumbleMates/cmd"
 )
 
-func askForName() string {
-	const MAX_NAME_LENGTH = 20
-
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your name (max 20 length): ")
-	name, _ := reader.ReadString('\n')
-
-	trimmedName := strings.TrimSpace(name)
-
-	if trimmedName == "" {
-		fmt.Println("Name cannot be empty. Please try again.")
-		return askForName()
-	}
-
-	if len(trimmedName) > MAX_NAME_LENGTH {
-		fmt.Println("Name cannot be longer than 20 characters. Please try again.")
-		return askForName()
-	}
-
-	return trimmedName
-}
-
 func main() {
 	if _, err := cmd.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v", err)
